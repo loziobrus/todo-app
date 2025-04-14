d20d5ed55c228f6ff4743dfe9cc26524a870dd4e
Sysak, (Yurii) <yurii.sysak@nn.cz>
2025-04-14 15:44:56 +0200

    app cleanup

--- todo-app.client/src/App.tsx
+++ todo-app.client/src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from "react";
 import { Typography } from "@mui/material";
 import { useAppDispatch } from "./store/store";
-import { deleteTask, editTask, fetchTasks } from "./store/services";
-import { Task, Status } from "./types";
+import { fetchTasks } from "./store/services";
 import TaskList from "./components/TaskList/TaskList";
 import NewTask from "./components/NewTask/NewTask";
 import "./App.css";
@@ -13,21 +12,6 @@ const App = () => {
   useEffect(() => {
     dispatch(fetchTasks());
   }, []);
-
-  const handleEditClick = async () => {
-    const updatedTask: Task = {
-      id: "f29a17dd-0ca8-430a-9633-1f47ebf1c801",
-      name: "Updated Task",
-      priority: 1,
-      status: Status.Completed,
-    };
-    dispatch(editTask(updatedTask));
-  };
-
-  const handleDeleteClick = async () => {
-    const taskId = "f29a17dd-0ca8-430a-9633-1f47ebf1c801";
-    dispatch(deleteTask(taskId));
-  };
 
   return (
     <div className="todo-app">
